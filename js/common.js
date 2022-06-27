9f30485ac3aaf074e871fc144578de4dfbebe2f2
jieunlee <liz950827@gmail.com>
2022-06-27 11:00:35 +0900

    common.js

---
+++ js/common.js
@@ -0,0 +1,8 @@
+import { hun } from "./module/variable.js";
+
+// <!-- *document.body -->
+document.body.setAttribute('style', `margin: 0; padding: 0; box-sizing: border-box;`);
+
+// <!-- *#root -->
+const root = document.getElementById('root');
+root.setAttribute('style', `width; ${hun}vw; height: ${hun}vh;`);
