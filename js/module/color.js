f9f46bfc56dfa56c5d3d0cceb5f8c1874409982c
jieunlee <liz950827@gmail.com>
2022-06-27 11:01:15 +0900

    color.js

---
+++ js/module/color.js
@@ -0,0 +1,10 @@
+const colorObj = {
+  colorBk: '#261D23',
+  colorDp: '#8C4D88',
+  colorGp: '#734C70',
+  colorPp: '#F2B6EE',
+  colorGr: '#81798C',
+  colorFf: '#fff'
+}
+
+export default colorObj;
